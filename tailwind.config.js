33e35599be4567426c12b44620d0515cd94e1cde
brian-backend <brian@funnull.com>
2024-03-20 17:41:02 +0700

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,30 @@
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/layouts/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/content/**/*.{md, mdx}',
   ],
   theme: {
+    screens: {
+      sm: "540px",
+      md: "768px",
+      lg: "992px",
+      xl: "1280px",
+      "2xl": "1400px",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#ffffff",
+        text: "#747272",
+        dark: "#747272",
+        cred: "#FD2C2F",
+        body: "#000000"
       },
+      fontFamily: {
+        primary: ['var(--font-pingfang)'],
+        secondary: ['var(--font-alimama)', 'MS Gothic', 'Simsun'],
+        title: ['var(--font-title)', 'var(--font-pingfang)','MS Gothic', 'Simsun']
+      }
     },
   },
   plugins: [],
